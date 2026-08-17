1c5df6a881eda2a1d97941e83f563669887342c7
Lavelle_Ali <lavelleali7@gmail.com>
2026-08-16 20:09:32 -0400

    Update: minor styling fixes

--- app/select/page.js
+++ app/select/page.js
@@ -8,10 +8,10 @@ const Select = () => {
       <NavBar />
 
       <div className="absolute top-10 left-8 text-left mt-5">
-        <h1 className="text-base font-semibold leading-[24px] tracking-tight">
+        <h1 className="text-base font-semibold leading-6 tracking-tight">
           A.I. ANALYSIS
         </h1>
-        <p className="text-sm mt-1 text-[#000000] uppercase leading-[24px]">
+        <p className="text-sm mt-1 text-[#000000] uppercase leading-6">
           A.I. has estimated the following.
           <br />
           Fix estimated information if needed.
@@ -29,28 +29,28 @@ const Select = () => {
             
           <div className="flex items-center justify-center col-start-2">
             <a href="/summary">
-              <button className="w-[153.88px] h-[153.88px] bg-gray-200 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 cursor-pointer font-semibold leading-[24px] tracking-tight uppercase hover:scale-[1.05] transition-transform duration-300">
+              <button className="w-[153.88px] h-[153.88px] bg-gray-200 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 cursor-pointer font-semibold leading-6 tracking-tight uppercase hover:scale-[1.05] transition-transform duration-300">
                 <span className="transform -rotate-45">Demographics</span>
               </button>
             </a>
           </div>
 
           <div className="flex items-center justify-center row-start-2 col-start-1">
-            <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 font-semibold leading-[24px] tracking-tight uppercase cursor-not-allowed">
+            <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 font-semibold leading-6 tracking-tight uppercase cursor-not-allowed">
               <span className="transform -rotate-45">Cosmetic Concerns</span>
             </button>
           </div>
 
           <div className="flex items-center justify-center row-start-2 col-start-3">
 
-            <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 font-semibold leading-[24px] tracking-tight uppercase cursor-not-allowed">
+            <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 font-semibold leading-6 tracking-tight uppercase cursor-not-allowed">
               <span className="transform -rotate-45">Skin Type Details</span>
             </button>
           </div>
 
           <div className="flex items-center justify-center row-start-3 col-start-2">
 
-            <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 font-semibold leading-[24px] tracking-tight uppercase cursor-not-allowed">
+            <button className="w-[153.88px] h-[153.88px] bg-gray-100 hover:bg-gray-300 transform rotate-45 flex items-center justify-center -m-5 font-semibold leading-6 tracking-tight uppercase cursor-not-allowed">
               <span className="transform -rotate-45">Weather</span>
             </button>
           </div>
@@ -62,7 +62,7 @@ const Select = () => {
         <div className="flex justify-between max-w-full mx-auto px-13 md:px-9">
           <a href="/results">
             <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
-              <span className="rotate-[-45deg] text-xs font-semibold sm:hidden">
+              <span className="-rotate-45 text-xs font-semibold sm:hidden">
                 BACK
               </span>
             </div>
